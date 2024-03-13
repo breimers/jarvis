68c629db93a811221eb6aed878be58a73c801fa2
breimers <b.a.reimers@gmail.com>
2024-03-12 18:08:13 -0700

    added command execution and chat history saving

--- chat.py
+++ chat.py
@@ -1,4 +1,8 @@
 import os
+import json
+import shlex
+import subprocess
+from datetime import datetime
 from typing import Any, List, Mapping, Optional
 from llama_cpp import Llama
 from langchain.vectorstores import Chroma
@@ -11,6 +15,8 @@ from langchain_core.language_models.llms import LLM
 
 DEFAULT_INTENTS = {
     "RAG": ["search", "find", "query", "retrieve", "look", "lookup", "research"],
+    "EXEC": ["exec", "execute", "run", "command", "cmd"],
+    "SAVE": ["/save", "/keep"]
 }
 
 
@@ -55,7 +61,13 @@ class ChatMessage:
     def __init__(self, actor, content) -> None:
         self.actor = actor
         self.content = content
-        
+    
+    def dict(self) -> str:
+        return {"actor": str(self.actor), "content": str(self.content)}
+    
+    def json(self) -> str:
+        return json.dumps(self.dict())
+
 
 class ChatHistory:
     def __init__(self, system_message) -> None:
@@ -71,6 +83,12 @@ class ChatHistory:
     
     def add(self, actor, message):
         self.messages.append(ChatMessage(actor, message))
+    
+    def save(self, location="history"):
+        filename = os.path.join(os.path.abspath(location), f"{str(int(datetime.now().timestamp()))}.txt")
+        with open(filename, "w") as f:
+            json.dump([m.dict() for m in self.messages], f)
+        return filename
 
 
 class GenerationArgs:
@@ -91,7 +109,61 @@ class GenerationArgs:
         }
 
 
-class Chat:
+class IntentionManager:
+    def __init__(self, chat_bot, intention_map=None) -> None:
+        if intention_map is None:
+            self.intention_map = DEFAULT_INTENTS
+        else:
+            self.intention_map = intention_map
+        self.chat_bot = chat_bot
+
+    def infer(self, input):
+        discovered_intents = list()
+        for k, v in DEFAULT_INTENTS.items():
+            if any([word.lower() in input.lower() for word in v]):
+                discovered_intents.append(k)
+        return discovered_intents
+    
+    def handle(self, input):
+        intents = self.infer(input)
+        for intent in intents:
+            self.__getattribute__(intent)(input)
+    
+    #  * Capslocking chat plugin methods to distinguish from normal methods
+    #     as these will be moved to their own class at some point.
+    #  * These definitely don't belong here, IntentionManager should match 
+    #     the intention_map keys to plugins.
+    #  * ChatBot should not contain plugin logic either.
+    def RAG(self, input):
+        if hasattr(self.chat_bot, 'rag'):
+            answer = self.chat_bot.rag.query(str(input))
+            self.chat_bot.history.add('document-retrieval', answer)
+        self.chat_bot.history.add('system', f"Please summarize the document-retrieval results, while keeping in mind the last user message.")
+
+    def EXEC(self, input, timeout=300):
+        input = str(input)
+        if any([input.startswith(key) for key in self.intention_map["EXEC"]]):
+            command = input.split()[1]
+            self.chat_bot.history.add('executor', f"Running command: {command}\n\n")
+            cmd = shlex.split(command, posix=True)
+            try:
+                proc = subprocess.run(args=cmd, universal_newlines=True, capture_output=True, text=True, timeout=timeout)
+                stdout = proc.stdout
+                stderr = proc.stderr
+                self.chat_bot.history.add('executor', f"Result: {stdout}\nError: {stderr}")
+            except subprocess.TimeoutExpired:
+                self.chat_bot.history.add('executor', f"Error: System failed to respond within {timeout} seconds.")
+            except subprocess.SubprocessError as e:
+                self.chat_bot.history.add('executor', f"Error: {e}")
+            self.chat_bot.history.add('system', "Please summarize the executor results for the user.")
+            
+    def SAVE(self, input):
+        _ = input
+        filename = self.chat_bot.history.save()
+        self.chat_bot.history.add('system', f"Inform user that the current chat history was saved to {filename}.")
+
+
+class ChatBot:
     def __init__(
         self, 
         system_prompt="You are a helpful AI assistant.",
@@ -101,6 +173,7 @@ class Chat:
         max_tokens=512,
         model_path="/Users/breimers/Workshop/models/llm/dolphin-2.6-mistral-7b-dpo-laser-Q8_0.gguf",
         data_store="rag/source",
+        exec=False,
         context_length=16000,
         gpu_layers=-1
     ) -> None:
@@ -112,9 +185,15 @@ class Chat:
             top_p
         )
         self.load_model(model_path, context_length, gpu_layers)
+        self.intentions = IntentionManager(self)
+        # Move to Plugins later
         if data_store is not None:
             self.load_rag_pipeline(data_store)
+        # Move to Plugins later
+        if exec:
+            self.exec = True
     
+    # Move to Plugins once the class is written
     def load_rag_pipeline(self, data_store):
         llm_wrapper = LangchainWrapper(model_instance=self.model)
         self.rag = RAGPipeline(data_store, llm_wrapper)
@@ -127,21 +206,10 @@ class Chat:
             n_gpu_layers=gpu_layers,
             f16_kv=True,
         )
-    
-    def infer_intent(self, input):
-        discovered_intents = list()
-        for k, v in DEFAULT_INTENTS.items():
-            if any([word.lower() in input.lower() for word in v]):
-                discovered_intents.append(k)
-        return discovered_intents
         
     def call(self, actor, input):
         self.history.add(actor, input)
-        intent = self.infer_intent(input)
-        if "RAG" in intent:
-            if hasattr(self, 'rag'):
-                answer = self.rag.query(str(input))
-                self.history.add('document-retrieval', answer)
+        self.intentions.handle(input)
         res = self.model(
             self.history.create_prompt(), 
             **self.gen_args.dict()
@@ -152,11 +220,15 @@ class Chat:
 
     def start_shell(self):
         while True:
-            user_input = str(input("Enter Text >>  "))
-            assistant_response = self.call("user", user_input)
-            print(f"Assistant: {assistant_response}")
+            try:
+                user_input = str(input("Enter Text >>  "))
+                assistant_response = self.call("user", user_input)
+                print(f"Assistant << {assistant_response}")
+            except KeyboardInterrupt:
+                self.history.save()
+                break
 
 
 if __name__ == "__main__":
-    chat = Chat()
+    chat = ChatBot(exec=True)
     chat.start_shell()
